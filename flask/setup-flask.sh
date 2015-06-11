e6acc916949d764a7a77062d9bb7ee967b1dc130
Eric So <me@eric.so>
2015-06-11 12:08:21 -0700

    De-indent

--- flask/setup-flask.sh
+++ flask/setup-flask.sh
@@ -23,54 +23,54 @@ else
 
   # Populate file contents
   echo "from app import app
-  app.run(host='0.0.0.0', port=8080, debug=True)
-  " >> $1/run.py
+app.run(host='0.0.0.0', port=8080, debug=True)
+" >> $1/run.py
 
   echo "# Statement for enabling the development environment
-  DEBUG = True
+DEBUG = True
 
-  # Define the application directory
-  import os
-  BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+# Define the application directory
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-  # Define the database - we are working with
-  # SQLite for this example
-  SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-  DATABASE_CONNECT_OPTIONS = {}
+# Define the database - we are working with
+# SQLite for this example
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+DATABASE_CONNECT_OPTIONS = {}
 
-  # Application threads. A common general assumption is
-  # using 2 per available processor cores - to handle
-  # incoming requests using one and performing background
-  # operations using the other.
-  THREADS_PER_PAGE = 2
+# Application threads. A common general assumption is
+# using 2 per available processor cores - to handle
+# incoming requests using one and performing background
+# operations using the other.
+THREADS_PER_PAGE = 2
 
-  # Enable protection agains *Cross-site Request Forgery (CSRF)*
-  CSRF_ENABLED = True
+# Enable protection agains *Cross-site Request Forgery (CSRF)*
+CSRF_ENABLED = True
 
-  # Use a secure, unique and absolutely secret key for
-  # signing the data.
-  CSRF_SESSION_KEY = "secret"
+# Use a secure, unique and absolutely secret key for
+# signing the data.
+CSRF_SESSION_KEY = "secret"
 
-  # Secret key for signing cookies
-  SECRET_KEY = \"secret\"
-  " >> $1/config.py
+# Secret key for signing cookies
+SECRET_KEY = \"secret\"
+" >> $1/config.py
 
   echo "from flask import Flask, render_template
-  from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 
-  # Define the WSGI application object
-  app = Flask(__name__)
+# Define the WSGI application object
+app = Flask(__name__)
 
-  # Configurations
-  app.config.from_object('config')
+# Configurations
+app.config.from_object('config')
 
-  # Define the database object which is imported
-  # by modules and controllers
-  db = SQLAlchemy(app)
+# Define the database object which is imported
+# by modules and controllers
+db = SQLAlchemy(app)
 
 
-  # Put views import after app creation to avoid circular import
-  from app import views, models
-  " >> $1/app/__init__.py
+# Put views import after app creation to avoid circular import
+from app import views, models
+" >> $1/app/__init__.py
 
 fi
